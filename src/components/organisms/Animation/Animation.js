75435de3c276c24aa60e6880c1b50f2f68f63ab8
Wioletta Miazga <wioletta.miazga1992@gmail.com>
2024-04-22 14:44:52 +0200

    animation library

--- src/components/organisms/Animation/Animation.js
+++ src/components/organisms/Animation/Animation.js
@@ -1,225 +1,101 @@
-import React, { useEffect, useState } from 'react';
-import Canvas from 'components/organisms/Canvas/Canvas';
+import { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from 'tsparticles-slim'; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+import { AnimationConfig } from './Animation-config';
 
-let hue = 0;
+const Animation = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
 
-const Animation = (props) => {
-  const [ctx, setCtx] = useState(null);
-  const [canvasWidth, setCanvasWidth] = useState(null);
-  const [canvasHeight, setCanvasHeight] = useState(null);
-  const chars =
-    'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
-  const letters = chars.split('');
-  const [particles, setParticles] = useState([]);
-  let hue = 1;
-
-  // Font-size remains constant, so similarly move outside draw
-  const establishContext = (ctx) => {
-    setCtx(ctx);
-  };
-
-  const establishCanvasWidth = (width) => {
-    setCanvasWidth(width);
-  };
-
-  const establishCanvasHeight = (height) => {
-    setCanvasHeight(height);
-  };
-
-  const fontSize = 10;
-  // Setting up the columns
-  const columns = canvasWidth ? canvasWidth / fontSize : 10;
-  // Setting up the drops
-  const drops = [];
-  for (let i = 0; i < columns; i++) {
-    drops[i] = 1;
-  }
-
-  const updateParticle = () => {
-    const updatedParticles = particles.map((particle) => {
-      const { x, y, speedX, speedY } = particle;
-      let { directionX, directionY } = particle;
-      let dx = x + speedX * hue * directionX;
-      let dy = y + speedY * hue;
-      if (dx < 0 || dx > establishCanvasWidth) {
-        dx = 0;
-        directionX = !directionX;
-        dx -= speedX * hue * directionX;
-      }
-
-      const updatedParticle = { ...particle, x: dx, y: dy };
-      return updatedParticle;
-    });
-    // setParticles(updatedParticles);
-    return updatedParticles;
-  };
-
-  // const updateParticle = (particle) => {
-  //   const { x, y, speedX, speedY } = particle;
-  //   const dx = x + speedX;
-  //   const dy = y + speedY;
-
-  //   const updatedParticle = { ...particle, x: dx, y: dy };
-  //   return updatedParticle;
-  // };
-
-  const generateParticle = () => {
-    const newParticle = {
-      x: Math.random() * ctx.canvas.width,
-      y: Math.random() * ctx.canvas.height,
-      size: Math.random() * 10 + 3,
-      color: `hsl(${hue}, 100%, 50%)`,
-      speedX: Math.random() * 3 - 1.5,
-      speedY: Math.random() * 3 - 1.5,
-      directionX: Math.random() > 0.5 ? 1 : -1,
-      directionY: Math.random() > 0.5 ? 1 : -1,
-    };
-    // hue = 2;
-    console.log(newParticle);
-    return newParticle;
-  };
-
-  // const generateParticle = (x, y) => {
-  //   console.log('generowanie', x, y);
-  //   let generatedParticles = [];
-  //   for (let i = 0; i < 10; i++) {
-  //     console.log('for');
-  //     const newParticle = {
-  //       x: x * Math.random(),
-  //       y: y * Math.random(),
-  //       size: Math.random() * 10 + 1,
-  //       color: `hsl(${hue}, 100%, 50%)`,
-  //       speedX: Math.random() * 3 - 1.5,
-  //       speedY: Math.random() * 3 - 1.5,
-  //     };
-  //     hue += 2;
-  //     generatedParticles = [...generatedParticles, newParticle];
-  //     console.log(generatedParticles, 'generatedParticles');
-  //   }
-  //   return generatedParticles;
-  // };
-
-  // const draw = () => {
-  //   if (ctx) {
-  //     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  //     hue++;
-  //     if (particles.length > 0) {
-  //       const updatedParticles = particles.map((particle) =>
-  //         updateParticle(particle),
-  //       );
-  //       if (updatedParticles.length > 0) {
-  //         // setParticles([...updatedParticles]);
-  //         updatedParticles.forEach((particle) => {
-  //           const {
-  //             x,
-  //             y,
-  //             size,
-  //             speedX,
-  //             speedY,
-  //             directionX,
-  //             directionY,
-  //             color,
-  //           } = particle;
-  //           ctx.beginPath();
-  //           ctx.fillStyle = color;
-  //           ctx.arc(x, y, size, 0, Math.PI * 2);
-
-  //           ctx.fill();
-  //         });
-  //       }
-
-  //       // if (x < 0 || x > ctx.canvas.width) {
-  //       //   console.log('cząstka opuszcza x');
-  //       //   directionX = !directionX;
-  //       // }
-  //       // if (y < 0 || y > ctx.canvas.height) {
-  //       //   console.log('opuszcza Y');
-  //       //   directionY = !directionY;
-  //       // }
-  //       // if (directionX === -1) {
-  //       //   x -= speedX * hue * directionX;
-  //       // } else {
-  //       //   x += speedX * hue * directionX;
-  //       // }
-
-  //       // if (directionY === -1) {
-  //       //   y -= speedY * hue * directionY;
-  //       // } else {
-  //       //   y += speedY * hue * directionY;
-  //       // }
-  //     }
-  //   }
-  // };
-
-  const draw = (particles) => {
-    if (ctx) {
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      hue++;
-      const updatedParticles = updateParticle();
-      if (updatedParticles.length < 10) {
-        generateParticle();
-      }
-      if (updatedParticles.length > 0) {
-        updatedParticles.forEach(
-          ({ x, y, size, speedX, speedY, directionX, directionY }) => {
-            ctx.beginPath();
-            const color = `hsl(${hue}, 100%, 50%)`;
-            ctx.fillStyle = color;
-            // if (x < 0 || x > ctx.canvas.width) {
-            //   console.log('cząstka opuszcza x');
-            //   directionX = !directionX;
-            // }
-            // if (y < 0 || y > ctx.canvas.height) {
-            //   console.log('opuszcza Y');
-            //   directionY = !directionY;
-            // }
-            // if (directionX === -1) {
-            //   x -= speedX * hue * directionX;
-            // } else {
-            //   x += speedX * hue * directionX;
-            // }
-
-            // if (directionY === -1) {
-            //   y -= speedY * hue * directionY;
-            // } else {
-            //   y += speedY * hue * directionY;
-            // }
-
-            ctx.arc(x, y, size, 0, Math.PI * 2);
-
-            ctx.fill();
-          },
-        );
-      }
-    }
-  };
-
-  useEffect(() => {
-    draw();
-    updateParticle();
-  }, [particles]);
-
-  const handleMouseMove = (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-
-    let newParticles = [];
-
-    for (let i = 0; i < 3; i++) {
-      const newParticle = generateParticle();
-      newParticles = [...newParticles, newParticle];
-    }
-    setParticles([...particles, ...newParticles]);
-  };
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
 
   return (
-    <Canvas
-      draw={draw}
-      updateParticle={updateParticle}
-      establishContext={establishContext}
-      establishCanvasWidth={establishCanvasWidth}
-      establishCanvasHeight={establishCanvasHeight}
-      onClick={handleMouseMove}
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: '#000',
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: false,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: 'random',
+            animation: {
+              enable: true,
+              speed: 60,
+            },
+          },
+          links: {
+            color: '#ffffff',
+            distance: 150,
+            enable: false,
+            opacity: 0.5,
+            width: 1,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outModes: {
+              default: 'bounce',
+            },
+            random: false,
+            speed: 0.3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 200,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
     />
   );
 };
